73ef7f782964b0543fcbcc6ac493cd98c40262e8
V1def <vaniya8871@gmail.com>
2022-04-18 22:35:09 +0300

    Update PWA files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,6 @@ const config = {
     [
       '@docusaurus/plugin-pwa',
       {
-        debug: true,
         offlineModeActivationStrategies: [
           'appInstalled',
           'standalone',
@@ -81,12 +80,48 @@ const config = {
           {
             tagName: 'link',
             rel: 'icon',
-            href: 'img/logo.svg',
+            href: '/img/logo.png',
           },
           {
             tagName: 'link',
             rel: 'manifest',
-            href: 'manifest.json',
+            href: '/manifest.json',
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: '#000',
+          },
+          {
+            tagName: 'link',
+            rel: 'apple-touch-icon',
+            href: '/img/logo.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'mask-icon',
+            href: '/img/logo.svg',
+            color: 'rgb(37, 194, 160)',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileImage',
+            content: '/img/logo.png',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileColor',
+            content: '#000',
           },
         ],
       }
